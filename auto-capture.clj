c65e7a669d9a5f9a8dd406bd3f4c07eb5e092b60
luc <luc@forgotten.name>
2023-11-07 04:26:45 -0500

    Changed auto-capture to save screenshots to json hypergraph

--- auto-capture.clj
+++ auto-capture.clj
@@ -1,7 +1,11 @@
 #!/usr/bin/env bb
 
 (require '[babashka.process :refer [shell]])
-(import '(java.text SimpleDateFormat)
+(require '[cheshire.core :as json])
+(require '[clojure.java.io :as io])
+(import '(java.util Base64 UUID)
+         '(java.nio.file Files Paths)
+         '(java.text SimpleDateFormat)
          '(java.util Date))
 
 ;; Define the destination folder on the desktop
@@ -10,18 +14,67 @@
 ;; Create the directory if it does not exist
 (shell "mkdir" "-p" destination-folder)
 
-;; Function to generate a timestamp string
-(defn timestamp-str []
-  (let [sdf (SimpleDateFormat. "yyyy-MM-dd_HH-mm-ss")]
+;; Function to generate a timestamp string in ISO 8601 format
+(defn timestamp-iso-str []
+  (let [sdf (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss'Z'")]
     (.format sdf (Date.))))
 
-;; Function to take a screenshot and save it to the specified folder with the frame index
-(defn capture-screenshot []
-  (let [filename (str (timestamp-str) ".png")
-        path (str destination-folder filename)]
-    (shell "screencapture" "-x" path)))
+;; Function to generate a UUID v4 string
+(defn uuid-v4 []
+  (.toString (UUID/randomUUID)))
 
-;; Main loop to take screenshots every 5 seconds
+;; Function to convert image to base64
+(defn image-to-base64 [path]
+  (try
+    (let [file-path (Paths/get path (make-array String 0))]
+      (let [file-content (Files/readAllBytes file-path)]
+        (.encodeToString (Base64/getEncoder) file-content)))
+    (catch Exception e
+      (println "Error converting image to Base64:" (.getMessage e))
+      nil))) ;; Return nil to indicate failure
+
+;; Function to save a JSON hypergraph to a file
+(defn save-json-hypergraph [json-object path]
+  (with-open [writer (io/writer path)]
+    (.write writer (json/generate-string json-object))
+    (.flush writer)))
+
+;; Function to save screenshot
+(defn capture-screenshot []
+  (let [temp-file (str "/tmp/" (uuid-v4) ".png")]
+    (println "Capturing screenshot to:" temp-file)
+    (shell "screencapture" "-x" temp-file)
+    (try
+      (let [file (io/file temp-file)]
+        (when (.exists file)
+          (println "Screenshot saved to:" (.getAbsolutePath file))
+          (let [base64 (image-to-base64 temp-file)]
+  ;;          (println "Base64 string:" base64) ;; This will print the Base64 string
+            base64)))
+      (catch Exception e
+        (println "Error capturing screenshot:" (.getMessage e)))
+      (finally
+        (println "Deleting temporary file:" temp-file)
+        (shell "rm" "-f" temp-file)))))
+
+
+
+(defn update-hypergraph [base64-image]
+;;  (println "Updating hypergraph with new image...") ;; Debugging line
+  (when (nil? base64-image)
+    (println "No image data to update.")) ;; Check for nil Base64 string
+  (let [hypergraph-path (str destination-folder "hypergraph.json")
+        hypergraph (if (.exists (io/file hypergraph-path))
+                     (json/parse-string (slurp hypergraph-path) true)
+                     {:nodes [] :hyperedges []})
+        node {:id (uuid-v4) :type "screenshot" :data base64-image :time (timestamp-iso-str)}
+        updated-hypergraph (update hypergraph :nodes conj node)]
+;;    (println "Saving updated hypergraph:" updated-hypergraph) ;; Debugging line
+    (save-json-hypergraph updated-hypergraph hypergraph-path)))
+
+
+;; Main loop to take screenshots every 5 seconds and save them as Base64 in a JSON hypergraph
 (dotimes [_ 1000]
-  (capture-screenshot)
+  (update-hypergraph (capture-screenshot))
   (Thread/sleep 5000)) ;; Sleep for 5000 milliseconds or 5 seconds
+
