4be20f58bbdd5d21ba300c280da8cdb69f467a92
luc <luc@forgotten.name>
2024-01-23 21:57:21 -0500

    recursive link scraping

--- scrape.py
+++ scrape.py
@@ -1,14 +1,29 @@
 import requests
 import lxml.html
 
+tld = "https://leginfo.legislature.ca.gov"
 url = "https://leginfo.legislature.ca.gov/faces/codes.xhtml"
-response = requests.get(url)
 
-# Parse the HTML content with lxml
-tree = lxml.html.fromstring(response.content)
+def scrape_links(url):
+    response = requests.get(url)
+    tree = lxml.html.fromstring(response.content)
+    for a_tag in tree.xpath('//*[@id="codestocheader"]//a'):
+        link_url = a_tag.get('href')
+        text = a_tag.text_content().strip()
+        print(f"URL: {link_url}, \nText: {text}\n")
+        scrape_links(tld + link_url)
+    for a_tag in tree.xpath('//*[@id="codestreeForm2"]//a'):
+        link_url = a_tag.get('href')
+        text = a_tag.text_content().strip()
+        print(f"URL: {link_url}, \nText: {text}\n")
+        scrape_links(tld + link_url)
+    for a_tag in tree.xpath('//*[@id="expandedbranchcodesid"]//a'):
+        link_url = a_tag.get('href')
+        text = a_tag.text_content().strip()
+        print(f"URL: {link_url}, \nText: {text}\n")
+        scrape_links(tld + link_url)
+    for law_sections in tree.xpath('//*[@id="manylawsections"]'):
+        text = law_sections.text_content().strip()
+        print(text)
 
-# Find all anchor tags and print their href attribute and text content
-for a_tag in tree.xpath('//a'):
-    url = a_tag.get('href')
-    text = a_tag.text_content().strip()
-    print(f"URL: {url}, Text: {text}")
+scrape_links(url)
