e47c019cc5b54cfde460c01102d3170bbf64cad0
Luc Chartier <luc@sanative.ai>
2024-02-28 11:08:59 -0500

    added scrape_manylawsection

--- scrape2.py
+++ scrape2.py
@@ -16,7 +16,9 @@ class SeleniumScraper:
         self.chrome_options = self.setup_chrome_options()
         self.visited_links = set()
         self.law_section_links = set()
+        self.processed_manylaw_links = set()
         self.executor = ThreadPoolExecutor(max_workers=50)
+        self.manylaw_executor = ThreadPoolExecutor(max_workers=10)
         self.stop_event = threading.Event()
         signal.signal(signal.SIGINT, self.signal_handler)
 
@@ -46,6 +48,36 @@ class SeleniumScraper:
         except Exception as e:
             logging.error(f"Error extracting links: {e}")
             return set()
+    
+    def scrape_manylawsections(self, url):
+        logging.info(f"Scraping manylawsections at URL: {url}")
+        driver = self.setup_driver()
+        driver.get(url)
+        element = driver.find_element(By.ID, "manylawsections")
+        elements = element.find_elements(By.TAG_NAME, 'a')
+        for link in elements:
+            href = link.get_attribute("href")
+            # if href not in self.visited_links:
+            print(link.get_attribute("href"))
+            driver.execute_script("arguments[0].click();", link)
+            e = driver.find_element(By.ID, "codeLawSectionNoHead")
+            top_level_divs = e.find_elements(By.XPATH, "./div")
+            for div in top_level_divs:
+                text_transform_value = div.value_of_css_property("text-transform")
+                text_indent_value = div.value_of_css_property("text-indent")
+                display_value = div.value_of_css_property("display")
+                if text_transform_value == "uppercase":
+                    print(f"Title: {div.text}")
+                elif (text_indent_value != "0px"):
+                    print(f"Division: {div.text}")
+                elif (display_value == "inline"):
+                    print(f"Chapter: {div.text}")
+                else:
+                    part = div.find_element(By.TAG_NAME,"h6")
+                    law = div.find_element(By.TAG_NAME, "p")
+                    print(f"Part: {part.text}")
+                    print(f"Law: {law.text}")
+        driver.quit()
 
     def scrape_url(self, url):
         if self.stop_event.is_set():
@@ -90,8 +122,18 @@ class SeleniumScraper:
                 self.visited_links.update(new_links)
                 urls_to_scrape.extend(new_links)
 
+                links_to_remove = set()
+                for manylaw_link in manylaw_links:
+                    if manylaw_link not in self.processed_manylaw_links:
+                        self.manylaw_executor.submit(self.scrape_manylawsections, manylaw_link)
+                        self.processed_manylaw_links.add(manylaw_link)
+                        links_to_remove.add(manylaw_link)
+
+                manylaw_links -= links_to_remove
+
         self.stop_event.set()
         timer_thread.join()
+        self.manylaw_executor.shutdown(wait=True)
         print("Scraping completed")
         print(self.law_section_links)
         logging.info("Scraping done")
